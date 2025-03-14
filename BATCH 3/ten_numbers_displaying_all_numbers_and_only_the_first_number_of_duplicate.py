d77347d218a41ec24b879a5b9309005ecb95a4f3
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 19:31:36 +0800

    getting the ten numbers from the user

--- BATCH 3/ten_numbers_displaying_all_numbers_and_only_the_first_number_of_duplicate.py
+++ BATCH 3/ten_numbers_displaying_all_numbers_and_only_the_first_number_of_duplicate.py
@@ -1,3 +1,18 @@
 #Create a function that will get the 10 numbers from the user
+numbers = []
+unique_numbers = []
+
+print("Input 10 numbers: ")
+
+for i in range(10):
+    while True:
+            num = float(input(f"Enter number {i + 1}: "))
+            numbers.append(num)
+            break
+
 #Create a function that will get the first number of the duplicate number/s
+
+
+
 #Display all the numbers and the numbers with only one duplicate number/s
+

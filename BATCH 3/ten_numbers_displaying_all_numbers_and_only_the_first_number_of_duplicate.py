e59f0ed7e6df22bb5c2b9a323bbfa1ab79059390
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 19:40:49 +0800

    making it as a one specified task by using def main

--- BATCH 3/ten_numbers_displaying_all_numbers_and_only_the_first_number_of_duplicate.py
+++ BATCH 3/ten_numbers_displaying_all_numbers_and_only_the_first_number_of_duplicate.py
@@ -1,22 +1,27 @@
 #Create a function that will get the 10 numbers from the user
-numbers = []
-unique_numbers = []
 
-print("Input 10 numbers: ")
+def main():
+    numbers = []
+    unique_numbers = []
 
-for i in range(10):
-    while True:
-            num = float(input(f"Enter number {i + 1}: "))
-            numbers.append(num)
-            break
+    print("Input 10 numbers: ")
 
-#Create a function that will get the first number of the duplicate number/s
+    for i in range(10):
+        while True:
+                num = float(input(f"Enter number {i + 1}: "))
+                numbers.append(num)
+                break
 
-for num in numbers:
-      if num not in unique_numbers:
-            unique_numbers.append(num)
+    for num in numbers:                 #Create a function that will get the first number of the duplicate number/s
+        if num not in unique_numbers:
+                unique_numbers.append(num)
 
-#Display all the numbers and the numbers with only one duplicate number/s
+    print("\nAll inputted numbers:")    #Display all the numbers and the numbers with only one duplicate number/s
+    print(numbers)
 
-print(unique_numbers)
+    print("\nNumbers with duplicates removed (only the first entry)")
+    print(unique_numbers)
+
+if __name__ == "__main__":
+      main()
 

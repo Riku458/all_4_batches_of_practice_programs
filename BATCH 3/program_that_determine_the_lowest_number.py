2bf19c961c04e534cfdb1df1faf656d9c88c3550
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 20:10:25 +0800

    creating a function that will get the user input and stopping if it is invalid input

---
+++ BATCH 3/program_that_determine_the_lowest_number.py
@@ -0,0 +1,15 @@
+#Create a function that will get the numbers that the user wants and exit if the input is not valid
+
+numbers = []
+
+while True:
+    user_input = input("Enter a number (or a non-numerical character to exit the program): ")
+
+    try:
+        number = float(user_input)
+        numbers.append(number)
+    except ValueError:
+        print("Please put a valid input. Exiting the program.")
+        break
+
+#Create a function that will determine the lowest number

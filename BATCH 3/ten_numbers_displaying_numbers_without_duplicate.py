862b26fd512aba1a973ca874d1952e0f5a11c144
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 18:59:58 +0800

    getting the numbers of user want to input

--- BATCH 3/ten_numbers_displaying_numbers_without_duplicate.py
+++ BATCH 3/ten_numbers_displaying_numbers_without_duplicate.py
@@ -1,3 +1,15 @@
 #First is to get the 10 numbers from the user
+
+numbers = []
+print("Input 10 numbers:")
+for i in range(10):
+    while True:
+        num = float(input(f"Number {i + 1}: "))
+        numbers.append(num)
+        break
+        
 #Create a function that will find the number/s with duplicate and disregarding that number/s
+
+
+
 #Printing the numbers without duplicate

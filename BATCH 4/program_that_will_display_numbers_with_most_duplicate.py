2cf63e71b188a3bbeef8fc3195539447f1e9036f
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 21:48:24 +0800

    creating a function that will get the most duplicated numbers + if the numbers are valid or not

--- BATCH 4/program_that_will_display_numbers_with_most_duplicate.py
+++ BATCH 4/program_that_will_display_numbers_with_most_duplicate.py
@@ -6,14 +6,22 @@ while True:
     user_input = input("Enter a number (or any non-numerical charater to exit the program): ")
 
     try:
-        number = float(user_input)
+        number = float(user_input) 
         numbers.append(number)
     except ValueError:
         print("Invalid input. Exiting the program.")
         break
 
-#Create a function that will get the numbers of duplicates
+if not numbers:                                 #Create a function that will get the numbers of duplicates
 
+    print("Invalid input")
+else:
+    duplicates = len(numbers) != len(set(numbers))
 
+    if not duplicates:                          #Create a function that will get the most duplicates number/s
+        print("All numbers are unique.")
+    else:
+        most_duplicate_num = max(set(numbers),  key=numbers.count)
+        max_count = numbers.count(most_duplicate_num)
 
-#Create a function that will get the most duplicates number/s
+#Display the output

7ace3c747f301ee0593e4b7ddeb9f95887ddda97
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 20:59:11 +0800

    creating a function that will get the numbers want by the user

--- BATCH 4/program_that_will_display_the_highest_number.py
+++ BATCH 4/program_that_will_display_the_highest_number.py
@@ -1,2 +1,14 @@
-#Create a function that will get the numbers that the user wants
+#Create a function that will get the numbers that the user wants and exits that program
+
+numbers = []
+
+while True:
+    user_input = input("Enter a number (or a non-numerical character to exit the program): ")
+
+    try:
+        number = float(user_input)
+        numbers.append(number)
+    except ValueError:
+        print("Please put a valid input. Exiting the program.")
+
 #Create a function that will determine the highest number

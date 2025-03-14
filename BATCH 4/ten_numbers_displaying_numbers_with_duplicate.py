676e6ae56d41dd91fdfaf8976f280f47ad8d0bcc
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 21:16:51 +0800

    creating a function that will get the duplicate numbers + printing all the duplicate numbers

--- BATCH 4/ten_numbers_displaying_numbers_with_duplicate.py
+++ BATCH 4/ten_numbers_displaying_numbers_with_duplicate.py
@@ -12,4 +12,17 @@ for i in range(10):
             print("Please put a valid input")
 
 #Create a function that will find the number/s with duplicate
+
+duplicate_numbers = []             
+for num in numbers:
+    if numbers.count(num) > 1 and num is not duplicate_numbers:
+        duplicate_numbers.append(num)
+
 #Printing the numbers with duplicate
+
+if duplicate_numbers:              
+    print("The numbers that have duplicates: ")              
+    for num in duplicate_numbers:
+        print(num)
+else:
+    print(f"No duplicates")

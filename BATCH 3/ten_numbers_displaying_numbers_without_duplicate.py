d705daffc10e0985dfc4decac30f1eb8fcc6ebd4
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 19:14:35 +0800

    making it a specified code by making it as def main

--- BATCH 3/ten_numbers_displaying_numbers_without_duplicate.py
+++ BATCH 3/ten_numbers_displaying_numbers_without_duplicate.py
@@ -1,20 +1,22 @@
 #First is to get the 10 numbers from the user
+def main():
+    numbers = []
+    print("Input 10 numbers:")
+    for i in range(10):
+        while True:
+            num = float(input(f"Number {i + 1}: "))
+            numbers.append(num)
+            break
 
-numbers = []
-print("Input 10 numbers:")
-for i in range(10):
-    while True:
-        num = float(input(f"Number {i + 1}: "))
-        numbers.append(num)
-        break
-        
-#Create a function that will find the number/s with duplicate and disregarding that number/s
+    unique_numbers = []             #Create a function that will find the number/s with duplicate and disregarding that number/s
+    for num in numbers:
+        if numbers.count(num) == 1:
+            unique_numbers.append(num)
 
-unique_numbers = []
-for num in numbers:
-    if numbers.count(num) == 1:
-        unique_numbers.append(num)
+    if unique_numbers:              #Printing the numbers without duplicate
+        print(f"Numbers that don't have duplicate: {unique_numbers}")
+    else:
+        print(f"All numbers are duplicate")
 
-#Printing the numbers without duplicate
-
-print(unique_numbers)
+if __name__ == "__main__":
+    main()

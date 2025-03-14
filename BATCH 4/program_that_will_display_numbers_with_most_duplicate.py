19f05c386ade56d19e5de79f95e7afa91200fdee
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 21:31:37 +0800

    Pseudocode

---
+++ BATCH 4/program_that_will_display_numbers_with_most_duplicate.py
@@ -0,0 +1,3 @@
+#Create a function that will get the numbers of what the user wants
+#Create a function that will get the numbers of duplicates
+#Create a function that will get the most duplicates number/s

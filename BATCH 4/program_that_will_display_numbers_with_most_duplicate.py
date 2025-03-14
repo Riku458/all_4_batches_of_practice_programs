0f11185cbe631fa8b6facedf9654d10849138209
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 21:34:09 +0800

    creating a function that will get the number of what the user wants

--- BATCH 4/program_that_will_display_numbers_with_most_duplicate.py
+++ BATCH 4/program_that_will_display_numbers_with_most_duplicate.py
@@ -1,3 +1,19 @@
 #Create a function that will get the numbers of what the user wants
+
+numbers = []
+
+while True:
+    user_input = input("Enter a number (or any non-numerical charater to exit the program): ")
+
+    try:
+        number = float(user_input)
+        numbers.append(number)
+    except ValueError:
+        print("Invalid input. Exiting the program.")
+        break
+
 #Create a function that will get the numbers of duplicates
+
+
+
 #Create a function that will get the most duplicates number/s

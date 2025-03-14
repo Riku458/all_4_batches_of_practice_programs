77594a8a0319f9e1630229c8010fd69ef16bdde3
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 21:59:15 +0800

    creating a function that will get the numbers of what the user wants

--- BATCH 4/program_that_will_print_average.py
+++ BATCH 4/program_that_will_print_average.py
@@ -1,2 +1,14 @@
 #Create a function that will get the numbers that the user wants
+
+numbers = []
+
+while True:
+    user_input = input("Enter a number (or any non-numerical character to exit the program): ")
+
+    try:
+        number = float(user_input)
+        numbers.append(number)
+    except ValueError:
+        break
+
 #Create a function that will get the average of the numbers and display it

e02722df2e25a0d0c2d977768790e18e3467dafc
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 20:53:02 +0800

    removing the extra spaces

--- BATCH 3/program_that_sort_numbers_lowest_to_highest.py
+++ BATCH 3/program_that_sort_numbers_lowest_to_highest.py
@@ -1,6 +1,5 @@
 #Create a function that will get the input that the user wants and stops if the input is invalid
 
-
 numbers = []
 while True:
     user_input = input("Enter a number (or any non-numerical character to exit the program): ")
@@ -9,7 +8,6 @@ while True:
         numbers.append(number)
     except ValueError:
         break
-
 
 #Create a function that will sort the numbers from lowest to highest
 

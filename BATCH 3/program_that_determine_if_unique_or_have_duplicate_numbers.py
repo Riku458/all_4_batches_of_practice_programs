9d331691aa8ecb8d55abe048f202da1f2159ac6b
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 19:53:35 +0800

    testing if the function for getting the number is working + making it exit the program

--- BATCH 3/program_that_determine_if_unique_or_have_duplicate_numbers.py
+++ BATCH 3/program_that_determine_if_unique_or_have_duplicate_numbers.py
@@ -1,4 +1,22 @@
 #Create a function that will get the number from the user
+
+numbers = []
+
+while True:
+    user_input = input("Enter a number: ")
+
+    try:
+        number = float(user_input)
+    except ValueError:
+        print("Invalid input. Exiting the program.")
+        break
+
 #Check if the number is valid or not
+
+
+
 #Check if the number is unique or duplicate
+
+
+
 #Display output

65b06814484eb4846f2f5166757167e81a9990eb
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 20:47:19 +0800

    running the function of getting the input of what the users wants + exiting the program

--- BATCH 3/program_that_sort_numbers_lowest_to_highest.py
+++ BATCH 3/program_that_sort_numbers_lowest_to_highest.py
@@ -1,2 +1,14 @@
 #Create a function that will get the input that the user wants and stops if the input is invalid
+
+
+numbers = []
+while True:
+    user_input = input("Enter a number (or any non-numerical character to exit the program): ")
+    try:
+        number = float(user_input)
+        numbers.append(number)
+    except ValueError:
+        break
+
+
 #Create a function that will sort the numbers from lowest to highest

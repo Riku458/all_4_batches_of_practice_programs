a51c266a438542727dc1d3ea0eac4d6b41649036
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 20:56:28 +0800

    getting the numbers want by the user

--- BATCH 4/program_that_sort_numbers_highest_to_lowest.py
+++ BATCH 4/program_that_sort_numbers_highest_to_lowest.py
@@ -1,2 +1,12 @@
 #Create a function that will get the numbers by what the user wants
+
+numbers = []
+while True:
+    user_input = input("Enter a number (or any non-numerical character to exit the program): ")
+    try:
+        number = float(user_input)
+        numbers.append(number)
+    except ValueError:
+        break
+
 #Create a function that will sort the numbers from highest to lowest

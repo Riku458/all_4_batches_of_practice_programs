57fbf20364d58b410cc5c29568462fa131ed3f7c
Riku458 <jedrickpogi69@gmail.com>
2025-03-14 19:27:41 +0800

    Pseudocode

---
+++ BATCH 3/ten_numbers_displaying_all_numbers_and_only_the_first_number_of_duplicate.py
@@ -0,0 +1,3 @@
+#Create a function that will get the 10 numbers from the user
+#Create a function that will get the first number of the duplicate number/s
+#Display all the numbers and the numbers with only one duplicate number/s
